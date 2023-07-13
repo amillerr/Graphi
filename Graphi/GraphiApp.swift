9dcd01827506928fe0b25cd2e811e8236738577d
Tom Miller <62253468+amillerr@users.noreply.github.com>
2023-07-13 16:58:43 +0300

    Initial Commit

---
+++ Graphi/GraphiApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphiApp.swift
+//  Graphi
+//
+//  Created by Artem Axenov on 2023-07-13.
+//
+
+import SwiftUI
+
+@main
+struct GraphiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
